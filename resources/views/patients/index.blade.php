1d4ff2794b449aeee2f9f428b5e6f1e194a74fe2
anangj <anangjulyanto257@gmail.com>
2024-11-29 09:52:23 +0700

    add datatable in patient list

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -145,6 +145,26 @@
     </div>
 
     @push('scripts')
+        <script type="module">
+            // data table validation
+            $("#data-table, .data-table").DataTable({
+                dom: "<'grid grid-cols-12 gap-5 px-6 mt-6'<'col-span-4'l><'col-span-8 flex justify-end'f><'#pagination.flex items-center'>><'min-w-full't><'flex justify-end items-center'p>",
+                paging: true,
+                ordering: true,
+                info: false,
+                searching: true,
+                lengthChange: true,
+                lengthMenu: [10, 25, 50, 100],
+                language: {
+                    lengthMenu: "Show _MENU_ entries",
+                    paginate: {
+                        previous: `<iconify-icon icon="ic:round-keyboard-arrow-left"></iconify-icon>`,
+                        next: `<iconify-icon icon="ic:round-keyboard-arrow-right"></iconify-icon>`,
+                    },
+                    search: "Search:",
+                },
+            });
+        </script>
         <script>
             const selectedPatients = [];
 
